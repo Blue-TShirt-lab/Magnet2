89955b786f1ecdc2ca99dcdba460fa8233ef5b47
Blue-TShirt-lab <101032385+Blue-TShirt-lab@users.noreply.github.com>
2022-04-18 11:58:27 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿//libraries
+using System.IO;
+using Magnet2;
+
+//variables for reading
+Console.Write("Path to file: ");
+string path = Console.ReadLine();
+string line;
+
+//variables for "Space" game + token list
+string[] tokenListSpace = {
+    "import Space",
+    "menu(",
+    "menu.ScoreCounter(",
+    "menu.Go(",
+    "menu.Exit(",
+};
+bool isRun = false;
+bool isMenu = false;
+bool isScoreCounter = false;
+bool isGoOption = false;
+bool isExitOption = false;
+Space spaceGame = new Space();
+
+//reading file
+using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+{
+    while ((line = sr.ReadLine()) != null)
+    {
+        read(line);
+    }
+}
+
+//updating game
+void read(string codeLine)
+{
+    if (codeLine.Contains(tokenListSpace[0]))
+    {
+        isRun = true;
+    }
+    else if (codeLine.Contains(tokenListSpace[1]))
+    {
+        string IM = codeLine.Split('(').Last();
+        isMenu = bool.Parse(IM.Substring(0, IM.IndexOf(')')));
+    }
+    else if (codeLine.Contains(tokenListSpace[2]))
+    {
+        string ISC = codeLine.Split('(').Last();
+        isScoreCounter = bool.Parse(ISC.Substring(0, ISC.IndexOf(')')));
+    }
+    else if (codeLine.Contains(tokenListSpace[3]))
+    {
+        string IGO = codeLine.Split('(').Last();
+        isGoOption = bool.Parse(IGO.Substring(0, IGO.IndexOf(')')));
+    }
+    else if (codeLine.Contains(tokenListSpace[4]))
+    {
+        string IEO = codeLine.Split('(').Last();
+        isExitOption = bool.Parse(IEO.Substring(0, IEO.IndexOf(')')));
+    }
+    else
+        Console.WriteLine("ERROR! On the \"" + codeLine + "\" line");
+}
+
+
+Console.Write("Do you want to run a game? (y/n): ");
+string dywrg = Console.ReadLine();
+
+if (dywrg.Contains("y") || dywrg.Contains("Y"))
+    run();
+else { }
+
+//running
+void run()
+{
+    if (isRun)
+    {
+        spaceGame.game(isMenu, isScoreCounter, isGoOption, isExitOption);
+    }
+}
---
+++ Space.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Magnet2
+{
+    internal class Space
+    {
+        Random rnd = new Random();
+        string[] spaceShip = {
+            "                  !???J~",
+            "                ^P5J~:!PJ",
+            "               ~B7~7.:.:P?",
+            "            :J?B!^777777~PJJ7",
+            "           .J&&7^Y^::::~Y~B&B7.",
+            "           :#&J:5~^~~~~^!Y!#&#:  ",
+            "           .BP.7Y????7???5^?&P",
+            "           ?#^!^.:......:::.5P  ",
+            "   ?!     ^#?!Y::::::::::.^~:G5   ",
+            "  ?&BYY:  PB:!:::.~GGG~.:777^~&! .^^Y?.",
+            "  G&#YYB. P#^^~::.Y#YBG:^??7:!B.:PBB&&G. ",
+            " .B##G?B. ~&5^5:::BGYY#J^?7~:B7 ?GY&BG&Y   ",
+            " :#BB&GB.  Y&!~:.7#5Y5B#~77:YP  ~GBBGG#B.    ",
+            " ^#BPB##^  .BB::.5BYYYP#5:^!#^  !#BGGP##:     ",
+            " :#BPPGBBY~ ~&Y.:BPYY5PB#~.GJ ^YBGPGGGB#:     ",
+            " :#BPGGPGB#^ ?&!:BPYY5P#B:JG 7#BGPGGBG#G.     ",
+            "  G#GGGGGP#P!!BG:?#YYYG&J~#5J##PGGGBGG&J      ",
+            "  ?&GPGGGGB#&&#&J:BPYYG#~P&&&#GGGBBGG#G.      ",
+            "   Y#BGBBBBB##B#&!7#YY#YJ&&##BGBBGBB#P:       ",
+            "    ^JPG####&J.^BBY#GG#PB77G&BB##BG5!         ",
+            "       .:~~!~:  :7BYYYBJ:  ^777!~:.           ",
+            "                  ??77J.};"
+        };
+        string GoOption = "\n[1] - Go!";
+        string exitOption = "\n[2] - Exit.";
+        string choose = "";
+        int score;
+        bool IM1, IGO1, IEO1, ISC1;
+
+        public void game(bool IM, bool ISC, bool IGO, bool IEO)
+        {
+            for (int i = 0; i <= spaceShip.Length-1; i++)
+            {
+                Console.WriteLine(spaceShip[i]);
+                Thread.Sleep(250);
+            }
+            menu(IM, ISC, IGO, IEO);
+        }
+
+        void menu(bool IM, bool ISC, bool IGO, bool IEO)
+        {
+            IM1 = IM;
+            ISC1 = ISC;
+            IGO1 = IGO;
+            IEO1 = IEO;
+            if (IM)
+            {
+                if (ISC == true)
+                    Console.WriteLine("\nYour score is: " + score);
+                if (IGO == true)
+                    Console.WriteLine(GoOption);
+                if (IEO == true) 
+                    Console.WriteLine(exitOption);
+            }
+            choose = Console.ReadLine();
+            if (choose == "1")
+            {
+                if(rnd.Next(1, 3) == 2)
+                    Fight();
+                else if (rnd.Next(1, 3) == 1){
+                    Console.WriteLine("\nAll is good...");
+                    menu(IM1, ISC1, IGO1, IEO1);
+                }
+            }
+            else { }
+        }
+        void Fight() {
+            Console.WriteLine("\nFight!");
+            if (rnd.Next(1, 3) == 2){
+                if (ISC1 == false)
+                    Console.Write("You win!");
+                else
+                    Console.WriteLine("+20 score!");
+                score = score + 20;
+            }
+            else if (rnd.Next(1, 3) == 1 || rnd.Next(1, 3) == 3) {
+                if (ISC1 == false)
+                    Console.Write("You lose!");
+                else
+                    Console.WriteLine("-10 score");
+                score = score - 10;
+            }
+            menu(IM1, ISC1, IGO1, IEO1);
+        }
+    }
+}
